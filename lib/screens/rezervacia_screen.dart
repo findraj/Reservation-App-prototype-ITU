bac988aa4f6b11af6327e539229fa9017ccefb3c
aladix02 <xbotlo01@stud.fit.vut.cz>
2023-10-28 21:54:51 +0200

    rezervacia hodiny a potvrdit

--- lib/screens/rezervacia_screen.dart
+++ lib/screens/rezervacia_screen.dart
@@ -7,51 +7,115 @@ class RezervaciaScreen extends StatefulWidget {
   _ReservationScreenState createState() => _ReservationScreenState();
 }
 
-class _ReservationScreenState extends State<RezervaciaScreen>{
+class _ReservationScreenState extends State<RezervaciaScreen> {
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
+  String? _selectedTime;
+
+  List<String> availableTimes = [
+    "10:00",
+    "11:00",
+    "12:00",
+    "13:00",
+    "14:00",
+    "15:00",
+    "16:00",
+    "17:00"
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('TableCalendar - Basics'),
+        title: Text('Rezervácia'),
       ),
-      body: TableCalendar(
-        firstDay: DateTime.utc(2010, 10, 16),
-        lastDay: DateTime.utc(2030, 3, 14),
-        focusedDay: _focusedDay,
-        calendarFormat: _calendarFormat,
-        selectedDayPredicate: (day) {
-          // Use `selectedDayPredicate` to determine which day is currently selected.
-          // If this returns true, then `day` will be marked as selected.
+      body: Column(
+        children: [
+          Expanded(
+            child: TableCalendar(
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              focusedDay: _focusedDay,
+              calendarFormat: _calendarFormat,
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                if (!isSameDay(_selectedDay, selectedDay)) {
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay;
+                    _selectedTime = null;
+                  });
+                }
+              },
+              onFormatChanged: (format) {
+                if (_calendarFormat != format) {
+                  setState(() {
+                    _calendarFormat = format;
+                  });
+                }
+              },
+              onPageChanged: (focusedDay) {
+                _focusedDay = focusedDay;
+              },
+            ),
+          ),
+          Wrap(
+            spacing: 10.0,
+            children: availableTimes.getRange(0, 4).map((time) {
+              return _buildTimeButton(time);
+            }).toList(),
+          ),
+          SizedBox(height: 10.0),
+          Wrap(
+            spacing: 10.0,
+            children: availableTimes.getRange(4, 8).map((time) {
+              return _buildTimeButton(time);
+            }).toList(),
+          ),
+          SizedBox(height: 20.0),
+          ElevatedButton(
+            onPressed: () {
+              // Handle the confirmation action here
+              print('Date: $_selectedDay, Time: $_selectedTime');
+            },
+            child: Text('Potvrdiť'),
+          ),
+          SizedBox(height: 20.0),
+        ],
+      ),
+    );
+  }
 
-          // Using `isSameDay` is recommended to disregard
-          // the time-part of compared DateTime objects.
-          return isSameDay(_selectedDay, day);
-        },
-        onDaySelected: (selectedDay, focusedDay) {
-          if (!isSameDay(_selectedDay, selectedDay)) {
-            // Call `setState()` when updating the selected day
-            setState(() {
-              _selectedDay = selectedDay;
-              _focusedDay = focusedDay;
-            });
-          }
-        },
-        onFormatChanged: (format) {
-          if (_calendarFormat != format) {
-            // Call `setState()` when updating calendar format
-            setState(() {
-              _calendarFormat = format;
-            });
-          }
-        },
-        onPageChanged: (focusedDay) {
-          // No need to call `setState()` here
-          _focusedDay = focusedDay;
-        },
+  ElevatedButton _buildTimeButton(String time) {
+    return ElevatedButton(
+      onPressed: () {
+        setState(() {
+          _selectedTime = time;
+        });
+      },
+      child: Text(time),
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.resolveWith<Color>(
+          (states) {
+            if (states.contains(MaterialState.pressed) ||
+                (_selectedTime == time)) {
+              return Theme.of(context).primaryColor;
+            }
+            return Colors.grey[300]!;
+          },
+        ),
+        foregroundColor: MaterialStateProperty.resolveWith<Color>(
+          (states) {
+            if (states.contains(MaterialState.pressed) ||
+                (_selectedTime == time)) {
+              return Colors.white;
+            }
+            return Colors.black;
+          },
+        ),
       ),
     );
   }
